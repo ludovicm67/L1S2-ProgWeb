8a7e2c3ae73da9f7b5d408aa50d3142993f763f9
ludovicm67 <ludovicmuller1@gmail.com>
2016-05-05 00:20:06 +0200

    commentaires

--- TP4/1-ManipulationDOM/script.js
+++ TP4/1-ManipulationDOM/script.js
@@ -1,7 +1,9 @@
+// Les éléments auxquels on devra beaucoup avoir accès
 var foo = document.getElementById('foo');
 var bar = document.getElementById('bar');
 var out = document.getElementById('out');
 
+// Permet d'actualiser les indices des 'li'
 function refreshIndices() {
 	var li = bar.querySelectorAll('li');
 	for (var i = 0; i < li.length; i++) {
@@ -9,10 +11,12 @@ function refreshIndices() {
 	}
 }
 
+// On ajoute un cadre bleu au premier 'li' lors d'un clic sur #foo
 foo.addEventListener('click', function () {
 	bar.querySelector('li').style.border = "5px solid #2980b9";
 });
 
+// Si on passe a souris sur '#foo > em', on dupplique le second 'li'
 foo.querySelector('em').addEventListener('mouseover', function () {
 	var secondItem = bar.querySelectorAll('li')[1].cloneNode(true);
 	secondItem.addEventListener('click', function () {
@@ -24,6 +28,7 @@ foo.querySelector('em').addEventListener('mouseover', function () {
 	refreshIndices();
 });;
 
+// On ajoute l'évènement en cas de clic sur tous les 'li' présents au départ
 var li = bar.querySelectorAll('li');
 for (var i = 0; i < li.length; i++) {
 	li[i].dataset.indice = i;
@@ -34,19 +39,25 @@ for (var i = 0; i < li.length; i++) {
 	});
 }
 
+// En cas d'appuie sur la touche 'a', on ajoute un nouvel item
 document.onkeypress = function(e) {
+
 	var key;
 
 	if(window.event) key = e.keyCode;
 	else if(e.which) key = e.which;
 
+	// Si c'est bien la touche 'a'
 	if(String.fromCharCode(key) == 'a') {
 		var newItem = document.createElement('li');
+
+		// Ajout de l'événement en cas de clic
 		newItem.addEventListener('click', function () {
 			alert(this.dataset.indice);
 			this.parentNode.removeChild(this);
 			out.innerHTML += '<br>' + this.innerHTML;
 		});
+
 		newItem.textContent = prompt('Nouvel item :');
 		bar.appendChild(newItem);
 		refreshIndices();
